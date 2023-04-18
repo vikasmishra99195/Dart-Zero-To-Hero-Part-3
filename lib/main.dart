164282207aeb2816aebd5658681ecb87d04a628f
Vikas Mishra <vikasmishra99195@gmail.com>
2023-04-18 11:02:21 +0530

    List in Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,57 @@
 void main(){
 
+  // # List : List is an Ordered group of Objects, List is zero based index, in which the first element starts from zero
+
+  // ## Growable List
+
+  // var list1 = ["Vikas", "Arjun", "Krsna", "Shiv"];
+  // print(list1);
+  // print(list1[0]);
+  // print(list1[1]);
+  // print(list1[2]);
+  // print(list1[3]);
+
+  // var list2 = ["Vikas", 10, 20, "Shiv"];
+  // print(list2);
+
+  // var list3 = ["Vikas", 10, 20, "Shiv"];
+  // list3.add("Aman");                     ##  .add function is use dto add new element in list
+  // list3.add("Swapna");                   ##  . Hew also .add function is used to store new element
+  // print(list3);
+
+  // ## create blank list and Add element
+
+  // var names = [];
+  // names.add("Vikas");
+  // names.add("Shiv");
+  // names.add("Swapna");
+  // names.add("Carry");
+  // print(names);                          ## output is [Vikas, Shiv, Swapna, Carry]
+
+  //  ## .addAll function in list
+
+  // var list3 = ["Vikas", "swapna", "Carry", "Shiv"];
+  // var names = [];
+  // names.addAll(list3);                     ## .addAll() function is used store all the elements of a specific created list
+  // print(names);
+
+  // ## add an element in a specific index (Use .insert function) index format [0 , 1, 2, 3]
+  // ## insert()
+
+  // var names = ["Vikas", "Carry", "Shiv", "Anmol"];
+  // print(names);                             ## whithout use insert the output is [Vikas, Carry, Shiv, Anmol]
+
+  // var names = ["Vikas", "Carry", "Shiv", "Anmol"];
+  // names.insert(1, "Swapna");                ## After using insert() function, .insert(index, element); index = put the index no in which you want to change the element and in element = put the new element
+  // print(names);                             ## After using insert() the output is [Vikas, Swapna, Carry, Shiv, Anmol]
+
+  // ## insertAll()
+
+  // var list1 = ["Vikas", "Carry", "Shiv", "Anmol"];
+  // var names = ["Akash", "Arjun","Adarsh", "Archana"];
+  // names.insertAll(2, list1);                   ## insertAll() is used to store the element of an specific list
+  // print(names);
+
 
 
 }
